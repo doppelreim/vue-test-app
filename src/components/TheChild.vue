1030ce3d858826882891b0172f4cd97eb28addcc
dplrm <dplrm@box>
2018-05-30 07:52:01 +0200

    add ChildComponent with failing test-case

---
+++ src/components/TheChild.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    the childParam: {{ childParam }}
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class TheChild extends Vue {
+    childParam = 'initial';
+
+    created() {
+      this.childParam = this.$route.query.child;
+    }
+  }
+</script>
--- tests/unit/HelloWorld.spec.ts
+++ tests/unit/HelloWorld.spec.ts
@@ -1,13 +1,21 @@
 import { expect } from 'chai';
-import { shallowMount } from '@vue/test-utils';
+import { mount, shallowMount, createLocalVue } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
+import VueRouter from 'vue-router';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
+    const vueMock = createLocalVue();
+    vueMock.use(VueRouter);
+
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = mount(HelloWorld, {
+      router: new VueRouter(),
       propsData: { msg },
     });
+
+    console.log(wrapper.html());
+
     expect(wrapper.text()).to.include(msg);
   });
 });
