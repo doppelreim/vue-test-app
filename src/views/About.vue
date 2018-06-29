62fb3ebc4c2ef8ee05316a78e7a538faa4e16afb
dplrm <dplrm@box>
2018-06-29 09:46:20 +0200

    make about-route take a param, watch route-changes and log info

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <router-link to="/about/5">Take 5</router-link>
   </div>
 </template>
+
+<script lang="ts">
+  import { Component, Vue, Watch } from 'vue-property-decorator';
+  import { Route } from 'vue-router';
+
+  @Component
+  export default class extends Vue {
+    @Watch('$route')
+    private async onParamChanged(newRoute: Route, oldRoute: Route) {
+      console.log('oldRoute', oldRoute.params.id, typeof oldRoute.params.id);
+      console.log('newRoute', newRoute.params.id, typeof newRoute.params.id);
+    }
+  }
+</script>
